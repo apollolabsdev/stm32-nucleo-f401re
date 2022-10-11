d10eca309c33be0af9b63c60ad41a24d3426e172
apollolabsdev <104051566+apollolabsdev@users.noreply.github.com>
2022-10-11 20:24:41 +0300

    max7219

---
+++ Drivers/max7219-driver/target/debug/deps/max7219_driver-59f44833040e8a75.d
@@ -0,0 +1,5 @@
+/Users/omarhiari/Dropbox/My Mac (omars-mbp.lan)/Documents/GitHub/stm32-nucleo-f4-rust/Drivers/max7219-driver/target/debug/deps/max7219_driver-59f44833040e8a75.rmeta: src/lib.rs
+
+/Users/omarhiari/Dropbox/My Mac (omars-mbp.lan)/Documents/GitHub/stm32-nucleo-f4-rust/Drivers/max7219-driver/target/debug/deps/max7219_driver-59f44833040e8a75.d: src/lib.rs
+
+src/lib.rs:
